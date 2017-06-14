d64676d201ae16ae3344fc03aa9654297891f292
james tak <jtak93@gmail.com>
2017-06-13 18:57:47 -0700

    remove facebook error and add port logging

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -2,12 +2,7 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 var auth = require('../controllers/auth-controller');
-var config = require('../config').auth.facebook
 
-router.get('/facebook', passport.authenticate('facebook', config.options.scope));
-router.get('/facebook/callback', passport.authenticate('facebook', { successRedirect: '/'}))
-
-router.get('/facebook-token', passport.authenticate('facebook-token'), auth.success);
 router.get('/basic', passport.authenticate('basic', {session: true}), auth.success);
 
 module.exports = router;
